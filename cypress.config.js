6e88d460f808927df4d5cbf59195bedd9f0c3a2d
lucas-scandido <luccas.candido@outlook.com>
2022-09-13 21:39:53 -0300

    Iniciado testes de api e revisado padrão e boas práticas no código para evitar flaky test

--- cypress.config.js
+++ cypress.config.js
@@ -15,5 +15,5 @@ module.exports = defineConfig({
       json: false,
       timestamp: "mmddyyyy_HHMMss"
     }
-  }  
+  }
 });
--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,4 +1,4 @@
 Cypress.Commands.add('login', (userName, password) => {
     cy.get('input[formcontrolname="userName"]').type(userName)
-    cy.get('input[formcontrolname="password"]').type(password)
+    cy.get('input[formcontrolname="password"]').type(password, {log:false})
 })
